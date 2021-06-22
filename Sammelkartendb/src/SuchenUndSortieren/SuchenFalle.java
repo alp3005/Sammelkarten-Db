a2ae72ffb96224e746f5e56e631fcfdab7b868f0
NilsRokossa <84380435+NilsRokossa@users.noreply.github.com>
2021-06-22 09:38:36 +0200

    Fibonacci Suche
    Ich bin mir nicht sicher, ob das wirklich läuft, oder ob ich es überhaupt richtig verstanden habe, aber eclipse zeigt zumindest erstmal keine Fehler an

--- Sammelkartendb/src/SuchenUndSortieren/SuchenFalle.java
+++ Sammelkartendb/src/SuchenUndSortieren/SuchenFalle.java
@@ -15,26 +15,13 @@ public class SuchenFalle {
 		if(grenze >= arr.length) {
 			return (Card[]) cardList.toArray(); //gesuchter Wert nicht enthalten. Leeres Array wird zurückgegeben
 		}
-		if(at == 5) {
+		if(at == 2) { //Typ
 			if(wertString.compareToIgnoreCase(arr[grenze].getType()) > 0) {
 				BinarySearchFalle(arr, grenze+1, stop, at, wertString);
 			} else if(wertString.compareToIgnoreCase(arr[grenze].getType()) < 0 && start != grenze) {
 				BinarySearchFalle(arr, start, grenze - 1, at, wertString);
 			} else if(wertString.equals(arr[grenze].getType())) { //gesuchter Wert an Position grenze gefunden
-				cardList.add(arr[grenze]);
-				//Check, ob weitere Werte den Suchparametern entsprechen
-				for(int i = grenze; i > 0; i--) { //Werte unter grenze
-					if(wertString.equals(arr[i].getType())) {
-						cardList.add(arr[i]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
-				for(int j = grenze; j < arr.length; j++) { //Werte über grenze
-					if(wertString.equals(arr[j].getType())) {
-						cardList.add(arr[j]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
+				cardList = CheckForMoreString(arr, cardList, grenze, wertString);
 				return (Card[]) cardList.toArray();
 			} else {
 				return (Card[]) cardList.toArray(); //gesuchter Wert nicht gefunden. Leeres Array wird zurückgegeben
@@ -44,20 +31,80 @@ public class SuchenFalle {
 		return (Card[]) cardList.toArray();
 	}
 
-	static Card[] FibonacciSearchFalle(Trap[] traps, int i, int length, int at, String atString) {
+	static Card[] FibonacciSearchFalle(Trap[] arr, int i, int length, int at, String wertString) throws Exception {
+		if(arr.length == 0)
+			throw new Exception("Keine Karten in der Datenbank"); //Falls die Datenbank leer ist
+		List<Card> cardList = new Vector<Card>(0,1);
+		int offset = -1;
+		//Fibonacci Zahlen
+		int fibNr2 = 0;
+		int fibNr1 = 1;
+		int fibN = fibNr2 + fibNr1;
+		while(fibN < arr.length) { // fibN wird die kleinste Fibonaccizahl größer/gleich der Länge des Arrays zugewiesen
+			fibNr2 = fibNr1;
+			fibNr1 = fibN;
+			fibN = fibNr2 + fibNr1;
+		}
+		//Eigentliche Suche
+		if(at == 2) { //Typ
+			while (fibN > 1) {
+				int n = FibMin(offset+fibNr2, arr.length-1);
+				if(arr[n].getType().compareToIgnoreCase(wertString) < 0) { //Wenn der gesuchte Wert größer als der an Position fibNr2 ist, wird bis zum derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr1;
+					fibNr1 = fibNr2;
+					fibNr2 = fibN - fibNr1;
+					offset = n;
+				} else if(arr[n].getType().compareToIgnoreCase(wertString) > 0) { //Wenn der gesuchte Wert kleiner als der an Position fibNr2 ist, wird nach dem derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr2;
+					fibNr1 = fibNr1 - fibNr2;
+					fibNr2 = fibN - fibNr1;
+				} else { //Element gleich gesuchtem Wert an Position i gefunden
+					cardList = CheckForMoreString(arr, cardList, n, wertString); //Check, ob weitere Werte den Suchparametern entsprechen
+					return (Card[]) cardList.toArray();
+				}
+			}
+			if (fibNr1 == 1 && arr[arr.length-1].getType().equals(wertString)) { //Das letzte Element des Arrays wird geprüft
+				cardList.add(arr[arr.length-1]);
+				return (Card[]) cardList.toArray();
+			}
+			return (Card[]) cardList.toArray(); //gesuchter Wert nicht im Array gefunden, leeres Array zurückgegeben
+		} else {
+			throw new Exception("Gewähltes Attribut passt nicht zum Kartentyp Monster"); //Sollte im fertigen Programm nicht eintreten können
+		}
+	}
+	
+	private static int FibMin(int x, int y) {
+		if(x <= y)
+			return x;
+		else
+			return y;
+	}
+
+	static Card[] ExponentialSearchFalle(Trap[] arr, int i, int length, int at, String wertString) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	static Card[] ExponentialSearchFalle(Trap[] traps, int i, int length, int at, String atString) {
+	static Card[] InterpolationSearchFalle(Trap[] arr, int i, int length, int at, String wertString) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	static Card[] InterpolationSearchFalle(Trap[] traps, int i, int length, int at, String atString) {
-		// TODO Auto-generated method stub
-		return null;
+	private static List<Card> CheckForMoreString(Trap[] arr, List<Card> list, int n, String wertString) { //Prüft, ob die Werte nach und vor einem gegebenen index n ebenfalls zu den Suchparametern passen
+		list.add(arr[n]); //erstes gefundenens Element
+		for(int i = n; i > 0; i--) { //Werte unter n
+			if(wertString.equals(arr[i].getType())) {
+				list.add(arr[i]);
+			} else
+				break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+		}
+		for(int j = n; j < arr.length; j++) { //Werte über n
+			if(wertString.equals(arr[j].getType())) {
+				list.add(arr[j]);
+			} else
+				break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+		}
+		return list;
 	}
-
 	
 }
--- Sammelkartendb/src/SuchenUndSortieren/SuchenMonster.java
+++ Sammelkartendb/src/SuchenUndSortieren/SuchenMonster.java
@@ -23,20 +23,7 @@ public class SuchenMonster {
 			} else if(wertInt < arr[grenze].getAtk() && start != grenze) {
 				BinarySearchMonster(arr, start, grenze - 1, at, wertInt);
 			} else if(wertInt == arr[grenze].getAtk()) { //gesuchter Wert an Position grenze gefunden
-				cardList.add(arr[grenze]);
-				//Check, ob weitere Werte den Suchparametern entsprechen
-				for(int i = grenze; i > 0; i--) { //Werte unter grenze
-					if(wertInt == arr[i].getAtk()) {
-						cardList.add(arr[i]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
-				for(int j = grenze; j < arr.length; j++) { //Werte über grenze
-					if(wertInt == arr[j].getAtk()) {
-						cardList.add(arr[j]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
+				cardList = CheckForMoreInt(arr, cardList, grenze, at, wertInt); //Alle gefundenen Werte werden in die Liste geschrieben
 				return (Card[]) cardList.toArray();
 			} else {
 				return (Card[]) cardList.toArray(); //gesuchter Wert nicht gefunden. Leeres Array wird zurückgegeben
@@ -48,20 +35,7 @@ public class SuchenMonster {
 			} else if(wertInt < arr[grenze].getDef() && start != grenze) {
 				BinarySearchMonster(arr, start, grenze - 1, at, wertInt);
 			} else if(wertInt == arr[grenze].getDef()) { //gesuchter Wert an Position grenze gefunden
-				cardList.add(arr[grenze]);
-				//Check, ob weitere Werte den Suchparametern entsprechen
-				for(int i = grenze; i > 0; i--) { //Werte unter grenze
-					if(wertInt == arr[i].getDef()) {
-						cardList.add(arr[i]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
-				for(int j = grenze; j < arr.length; j++) { //Werte über grenze
-					if(wertInt == arr[j].getDef()) {
-						cardList.add(arr[j]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
+				cardList = CheckForMoreInt(arr, cardList, grenze, at, wertInt); //Alle gefundenen Werte werden in die Liste geschrieben
 				return (Card[]) cardList.toArray();
 			} else {
 				return (Card[]) cardList.toArray(); //gesuchter Wert nicht gefunden. Leeres Array wird zurückgegeben
@@ -73,20 +47,7 @@ public class SuchenMonster {
 			} else if(wertInt < arr[grenze].getLvl() && start != grenze) {
 				BinarySearchMonster(arr, start, grenze - 1, at, wertInt);
 			} else if(wertInt == arr[grenze].getLvl()) { //gesuchter Wert an Position grenze gefunden
-				cardList.add(arr[grenze]);
-				//Check, ob weitere Werte den Suchparametern entsprechen
-				for(int i = grenze; i > 0; i--) { //Werte unter grenze
-					if(wertInt == arr[i].getLvl()) {
-						cardList.add(arr[i]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
-				for(int j = grenze; j < arr.length; j++) { //Werte über grenze
-					if(wertInt == arr[j].getLvl()) {
-						cardList.add(arr[j]);
-					} else
-						break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
-				}
+				cardList = CheckForMoreInt(arr, cardList, grenze, at, wertInt); //Alle gefundenen Werte werden in die Liste geschrieben
 				return (Card[]) cardList.toArray();
 			} else {
 				return (Card[]) cardList.toArray(); //gesuchter Wert nicht gefunden. Leeres Array wird zurückgegeben
@@ -100,19 +61,157 @@ public class SuchenMonster {
 		return (Card[]) cardList.toArray();
 	}
 
-	static Card[] FibonacciSearchMonster(Monster[] monsters, int i, int length, int at, int atInt) {
+	static Card[] FibonacciSearchMonster(Monster[] arr, int i, int length, int at, int wertInt) throws Exception {
+		if(arr.length == 0)
+			throw new Exception("Keine Karten in der Datenbank"); //Falls die Datenbank leer ist
+		List<Card> cardList = new Vector<Card>(0,1);
+		int offset = -1;
+		//Fibonacci Zahlen
+		int fibNr2 = 0;
+		int fibNr1 = 1;
+		int fibN = fibNr2 + fibNr1;
+		while(fibN < arr.length) { // fibN wird die kleinste Fibonaccizahl größer/gleich der Länge des Arrays zugewiesen
+			fibNr2 = fibNr1;
+			fibNr1 = fibN;
+			fibN = fibNr2 + fibNr1;
+		}
+		//Eigentliche Suche
+		switch(at) {
+		case 3: //ATK
+			while (fibN > 1) {
+				int n = FibMin(offset+fibNr2, arr.length-1);
+				if(arr[n].getAtk() < wertInt) { //Wenn der gesuchte Wert größer als der an Position fibNr2 ist, wird bis zum derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr1;
+					fibNr1 = fibNr2;
+					fibNr2 = fibN - fibNr1;
+					offset = n;
+				} else if(arr[n].getAtk() > wertInt) { //Wenn der gesuchte Wert kleiner als der an Position fibNr2 ist, wird nach dem derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr2;
+					fibNr1 = fibNr1 - fibNr2;
+					fibNr2 = fibN - fibNr1;
+				} else { //Element gleich gesuchtem Wert an Position i gefunden
+					cardList = CheckForMoreInt(arr, cardList, n, at, wertInt); //Check, ob weitere Werte den Suchparametern entsprechen
+					return (Card[]) cardList.toArray();
+				}
+			}
+			if (fibNr1 == 1 && arr[arr.length-1].getAtk() == wertInt) { //Das letzte Element des Arrays wird geprüft
+				cardList.add(arr[arr.length-1]);
+				return (Card[]) cardList.toArray();
+			}
+			return (Card[]) cardList.toArray(); //gesuchter Wert nicht im Array gefunden, leeres Array zurückgegeben
+		case 4: //DEF
+			while (fibN > 1) {
+				int n = FibMin(offset+fibNr2, arr.length-1);
+				if(arr[n].getDef() < wertInt) { //Wenn der gesuchte Wert größer als der an Position fibNr2 ist, wird bis zum derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr1;
+					fibNr1 = fibNr2;
+					fibNr2 = fibN - fibNr1;
+					offset = n;
+				} else if(arr[n].getDef() > wertInt) { //Wenn der gesuchte Wert kleiner als der an Position fibNr2 ist, wird nach dem derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr2;
+					fibNr1 = fibNr1 - fibNr2;
+					fibNr2 = fibN - fibNr1;
+				} else { //Element gleich gesuchtem Wert an Position i gefunden
+					cardList = CheckForMoreInt(arr, cardList, n, at, wertInt); //Check, ob weitere Werte den Suchparametern entsprechen
+					return (Card[]) cardList.toArray();
+				}
+			}
+			if (fibNr1 == 1 && arr[arr.length-1].getDef() == wertInt) { //Das letzte Element des Arrays wird geprüft
+				cardList.add(arr[arr.length-1]);
+				return (Card[]) cardList.toArray();
+			}
+			return (Card[]) cardList.toArray(); //gesuchter Wert nicht im Array gefunden, leeres Array zurückgegeben
+		case 6: //Stufe
+			while (fibN > 1) {
+				int n = FibMin(offset+fibNr2, arr.length-1);
+				if(arr[n].getLvl() < wertInt) { //Wenn der gesuchte Wert größer als der an Position fibNr2 ist, wird bis zum derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr1;
+					fibNr1 = fibNr2;
+					fibNr2 = fibN - fibNr1;
+					offset = n;
+				} else if(arr[n].getLvl() > wertInt) { //Wenn der gesuchte Wert kleiner als der an Position fibNr2 ist, wird nach dem derzeitigen i das Array nicht weiter geprüft
+					fibN = fibNr2;
+					fibNr1 = fibNr1 - fibNr2;
+					fibNr2 = fibN - fibNr1;
+				} else { //Element gleich gesuchtem Wert an Position i gefunden
+					cardList = CheckForMoreInt(arr, cardList, n, at, wertInt); //Check, ob weitere Werte den Suchparametern entsprechen
+					return (Card[]) cardList.toArray();
+				}
+			}
+			if (fibNr1 == 1 && arr[arr.length-1].getLvl() == wertInt) { //Das letzte Element des Arrays wird geprüft
+				cardList.add(arr[arr.length-1]);
+				return (Card[]) cardList.toArray();
+			}
+			return (Card[]) cardList.toArray(); //gesuchter Wert nicht im Array gefunden, leeres Array zurückgegeben
+		default:
+			throw new Exception("Gewähltes Attribut passt nicht zum Kartentyp Monster"); //Sollte im fertigen Programm nicht eintreten können
+		}
+	}
+
+	private static int FibMin(int x, int y) {
+		if(x <= y)
+			return x;
+		else
+			return y;
+	}
+
+	static Card[] ExponentialSearchMonster(Monster[] arr, int i, int length, int at, int wertInt) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	static Card[] ExponentialSearchMonster(Monster[] monsters, int i, int length, int at, int atInt) {
+	static Card[] InterpolationSearchMonster(Monster[] arr, int i, int length, int at, int wertInt) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	static Card[] InterpolationSearchMonster(Monster[] monsters, int i, int length, int at, int atInt) {
-		// TODO Auto-generated method stub
-		return null;
+	private static List<Card> CheckForMoreInt(Monster[] arr, List<Card> list, int n, int at, int wertInt) { //Prüft, ob die Werte nach und vor einem gegebenen index n ebenfalls zu den Suchparametern passen
+		list.add(arr[n]); //erstes gefundenens Element
+		switch(at) {
+		case 3:
+			for(int i = n; i > 0; i--) { //Werte unter n
+				if(wertInt == arr[i].getAtk()) {
+					list.add(arr[i]);
+				} else
+					break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+			}
+			for(int j = n; j < arr.length; j++) { //Werte über n
+				if(wertInt == arr[j].getAtk()) {
+					list.add(arr[j]);
+				} else
+					break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+			}
+			break;	
+		case 4:
+			for(int i = n; i > 0; i--) { //Werte unter n
+				if(wertInt == arr[i].getDef()) {
+					list.add(arr[i]);
+				} else
+					break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+			}
+			for(int j = n; j < arr.length; j++) { //Werte über n
+				if(wertInt == arr[j].getDef()) {
+					list.add(arr[j]);
+				} else
+					break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+			}
+			break;
+		case 6:
+			for(int i = n; i > 0; i--) { //Werte unter n
+				if(wertInt == arr[i].getLvl()) {
+					list.add(arr[i]);
+				} else
+					break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+			}
+			for(int j = n; j < arr.length; j++) { //Werte über n
+				if(wertInt == arr[j].getLvl()) {
+					list.add(arr[j]);
+				} else
+					break; //Da die Werte vorher sortiert wurden, kann beim ersten nicht mehr übereinstimmenden Element abgebrochen werden
+			}
+			break;	
+		}
+		return list;
 	}
-	
+
 }
